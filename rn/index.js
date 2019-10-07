5dae26fd2ad2b6cd96a90218c3c4da8588e06dfc
binbinzhu <binbin.zhu@opay-inc.com>
2019-10-07 12:18:07 +0800

    index 增加按钮

--- rn/index.js
+++ rn/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Button,Text, View} from 'react-native';
 
 class HelloWorld extends React.Component {
+  componentDidUpdate(prevProps, prevState, snapshot){
+      console.log("componentDidUpdate")
+  }
+  componentDidMount(){
+  // alert("你好!")
+  console.log("zhubin===componentDidMount!")
+  }
+  _hello(){
+     alert("你好!")
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.hello}>Hello, World</Text>
+        <Text style={styles.hello}>Hello, zhubin</Text>
+      <Button title="按钮" style = {styles.btn} onPress={this._hello}></Button>
       </View>
     );
-  }
+}
 }
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  btn:{
+    fontSize: 10,
+    marginTop: 10,
   },
   hello: {
     fontSize: 20,

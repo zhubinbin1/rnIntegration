0c1ea504b68dc9eaf06e9fbbf6bd63e73d84e8da
binbinzhu <binbin.zhu@opay-inc.com>
2019-10-07 14:38:05 +0800

    增加phshy react-native-update

--- rn/index.js
+++ rn/index.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, Button,Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Button, Text, View} from 'react-native';
 
 class HelloWorld extends React.Component {
-  componentDidUpdate(prevProps, prevState, snapshot){
-      console.log("componentDidUpdate")
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate")
   }
-  componentDidMount(){
-  // alert("你好!")
-  console.log("zhubin===componentDidMount!")
+  componentDidMount() {
+    // alert("你好!")
+    console.log("zhubin===componentDidMount!")
   }
-  _hello(){
-     alert("你好!")
+  _hello() {
+    alert("你好!")
   }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>Hello, zhubin</Text>
-      <Button title="按钮" style = {styles.btn} onPress={this._hello}></Button>
-      </View>
-    );
-}
+    return (<View style={styles.container}>
+      <Text style={styles.hello}>Hello, zhubin</Text>
+      <Button title="按钮" style={styles.btn} onPress={this._hello}></Button>
+    </View>);
+  }
 }
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-  btn:{
+  btn: {
     fontSize: 10,
-    marginTop: 10,
+    marginTop: 10
   },
   hello: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
-  },
+    margin: 10
+  }
 });
 
 AppRegistry.registerComponent('rn_project', () => HelloWorld);
